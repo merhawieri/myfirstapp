f1674ef733695598d1d90589da51dfa3f03e0521
merhawieri <merakassala@gmail.com>
2018-06-18 02:25:33 -0400

    3.9 navigation and footer added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
+
+
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -13,4 +15,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+/= require jquery3
 //= require_tree .
