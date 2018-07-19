7609137ff67c0144d7e8be1f7170a2d1419fcb77
merhawieri <merakassala@gmail.com>
2018-07-19 17:25:16 -0400

    4.6 orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,11 @@ class OrdersController<ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @orders = Order.all
+  @orders = Order.includes(:product).all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
