36ef44e6b24c964daf19b24623141ba6e8167759
merhawieri <merakassala@gmail.com>
2018-07-19 17:46:51 -0400

    4,6 models orders updated

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,6 +1,8 @@
 class SimplePagesController < ApplicationController
   def index
   end
+  def show
+  end
   def landing_page
      @products = Product.limit(3)
         @featured_product = Product.first
