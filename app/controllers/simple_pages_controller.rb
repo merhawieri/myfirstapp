be8e3b6454ee5a3b0ac78e549ca8bb08225c4b77
merhawieri <merakassala@gmail.com>
2018-07-19 02:38:03 -0400

    4.5 views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,8 +2,8 @@ class SimplePagesController < ApplicationController
   def index
   end
   def landing_page
-
+     @products = Product.limit(3)
         @featured_product = Product.first
       end
-  
+
 end
