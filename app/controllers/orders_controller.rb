e1ff549e2c0b5f83a4a7cb25468bf0d30de1ecc0
merhawieri <merakassala@gmail.com>
2018-07-20 02:39:08 -0400

    4.6s

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController<ApplicationController
   end
 
   def show
-    @orders = Order.find(params[:id])
+  
   end
 
   def new
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-
+  @orders = Order.find(params[:id])
 
   end
 
