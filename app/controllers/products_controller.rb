cf34fdafb8f0513f15582876b600d945c3f80737
merhawieri <merakassala@gmail.com>
2018-08-02 14:33:59 -0400

    5.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,8 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+    format.html { redirect_back }fallback_location: {action: "show"}
+      #  /*--format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
