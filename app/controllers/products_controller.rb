1ff40b4eef25b080f1ee6e90278e770ca43d3b3d
merhawieri <merakassala@gmail.com>
2018-08-06 14:05:40 -0400

    5.2 product form and serch box

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-  @orders = Order.find(params[:id])
+  #@orders = Order.find(params[:id])
 
   end
 
