76f268271d0fa00c6cf4659a48f4993c6f9aa1e3
merhawieri <merakassala@gmail.com>
2018-08-07 01:19:07 -0400

    5.2

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,11 @@ class Product < ApplicationRecord
   has_many :orders
 
 def self.search(search_term)
-    if Rails.env.development?
-      Product.where("name LIKE ?", "%#{search_term}%")
-    else
-      Product.where("name ilike ?", "%#{search_term}%")
-    end
-  end
+  #  if Rails.env.development?
+    #  Product.where("name LIKE ?", "%#{search_term}%")
+    #else
+    #  Product.where("name ilike ?", "%#{search_term}%")
+    #end
+  #end
+   Product.where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
 end
