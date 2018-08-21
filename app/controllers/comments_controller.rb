99799ba45964cb196668a7bb0069776591ed9728
merhawieri <merakassala@gmail.com>
2018-08-21 15:38:30 -0400

    5.8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
 
     def destroy
     end
-    rivate
+    private
         def comment_params
           params.require(:comment).permit(:user_id, :body, :rating)
         end
