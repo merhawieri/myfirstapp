c542e75ad410e6716d15aadb57f301fec24bc5e8
merhawieri <merakassala@gmail.com>
2018-08-26 13:14:25 -0400

    5.10admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    if user.admin?
+       can :manage, :all
+     else
         can :manage, User, id: user.id
+        can :manage, Comment, id: user.id
 
+end
 
     # Define abilities for the passed in user here. For example:
     #
