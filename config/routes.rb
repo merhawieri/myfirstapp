6515303a5773584547d70b930f70d8c8ba380599
merhawieri <merakassala@gmail.com>
2018-08-27 01:00:11 -0400

    5.10

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ end
   get 'simple_pages/contact'
   get 'simple_pages/index'
 post 'simple_pages/thank_you'
-  root 'products#index'
+
+  root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+#simple_pages#landing_page'
+#root 'products#index'
