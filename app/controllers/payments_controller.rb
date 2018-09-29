59ba522b00cb89ea770843302a0c3769b62fe2d1
merhawieri <merakassala@gmail.com>
2018-09-29 01:54:06 -0400

    6.6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  before_action :authenticate_user!
   def create
         @product = Product.find(params[:product_id])
         @user = current_user
