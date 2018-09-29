d81a9df8721ee334ecb63682a11a491d5808455d
merhawieri <merakassala@gmail.com>
2018-09-29 12:23:21 -0400

    6.6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,31 +1,35 @@
 class PaymentsController < ApplicationController
-  def create
-      token = params[:stripeToken]
-      @product = Product.find(params[:product_id])
-      @user = current_user
 
-      # This will charge the user's card:
-      begin
-        charge = Stripe::Charge.create(
-          :amount => (@product.price * 100).to_i,
-          :currency => "usd",
-          :source => token,
-          :description => params[:stripeEmail],
-          :receipt_email => params[:stripeEmail]
-        )
+  def create
+        @product = Product.find(params[:product_id])
+        @user = current_user
+
+        token = params[:stripeToken]
+        begin
+            charge = Stripe::Charge.create(
+                amount: (@product.price*100).to_i,
+                currency: "usd",
+                source: token,
+                description: params[:stripeEmail]
+            )
+
         if charge.paid
-          Order.create(product_id: @product.id, user_id: @user.id,
-        total: @product.price.to_i)
-        UserMailer.order_created(@user,@product).deliver_now
+            Order.create(
+              product_id: @product.id,
+              user_id: @user.id,
+              total: @product.price
+            )
+            flash[:success] = "Your payment was processed successfully"
         end
 
-      rescue Stripe::CardError => e
-        # When the card has been declined
-        body = e.json_body
-        err = body[:error]
-        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-      end
-     end
+        rescue Stripe::CardError => e
+            body = e.json_body
+            err = body[:error]
+            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+        end
 
+        redirect_to product_path(@product)
+
+    end
 
 end
