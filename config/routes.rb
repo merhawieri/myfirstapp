7acb5560034c2588741c455e8013351d1e4d7bd6
merhawieri <merakassala@gmail.com>
2018-09-29 12:18:58 -0400

    6.6

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
   resources :comments
 end
   resources :users
-   resources :orders
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
 post 'simple_pages/thank_you'
  post 'payments/create'
-  get 'payments/create'
   root 'simple_pages#landing_page'
-  #resources :orders, only: [:index, :show, :create, :destroy]
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
