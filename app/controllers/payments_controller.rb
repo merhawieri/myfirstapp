c9acf28bafeccb97e381ffda3420cc1bfb48c967
merhawieri <merakassala@gmail.com>
2018-09-29 12:46:51 -0400

    6.6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,36 +1,31 @@
 class PaymentsController < ApplicationController
-
   def create
-        @product = Product.find(params[:product_id])
-        @user = current_user
+      token = params[:stripeToken]
+      @product = Product.find(params[:product_id])
+      @user = current_user
 
-        token = params[:stripeToken]
-        begin
-            charge = Stripe::Charge.create(
-                amount: (@product.price*100).to_i,
-                currency: "usd",
-                source: token,
-                description: params[:stripeEmail]
-            )
-
+      # This will charge the user's card:
+      begin
+        charge = Stripe::Charge.create(
+          :amount => (@product.price * 100).to_i,
+          :currency => "usd",
+          :source => token,
+          :description => params[:stripeEmail],
+          :receipt_email => params[:stripeEmail]
+        )
         if charge.paid
-            Order.create(
-              product_id: @product.id,
-              user_id: @user.id,
-              total: @product.price
-            )
-            flash[:success] = "Your payment was processed successfully"
+          Order.create(product_id: @product.id, user_id: @user.id,
+        total: @product.price.to_i)
+        UserMailer.order_created(@user,@product).deliver_now
         end
 
-        rescue Stripe::CardError => e
-            body = e.json_body
-            err = body[:error]
-            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-        end
+      rescue Stripe::CardError => e
+        # When the card has been declined
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      end
+     end
 
-        redirect_to product_path(@product)
 
-    end
-
-end
 end
