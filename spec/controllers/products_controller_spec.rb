d0a41c7c25533dcce461008711ae43a13e4022d6
merhawieri <merakassala@gmail.com>
2018-10-02 17:27:52 -0400

    6.6

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ProductsController, type: :controller do
   context 'unauthenticated user' do
     it 'cannot edit' do
-      product = FactoryBot.create(:product)
+      @product = FactoryBot.create(:product)
       get :edit, params:{id: product.id}
       expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to match("You need to sign in or sign up before continuing.")
