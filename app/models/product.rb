19ef528b104b698497188acf085ee62a581f98db
merhawieri <merakassala@gmail.com>
2018-10-10 01:06:30 -0400

    6.12

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,4 +19,20 @@ def lowest_rating_comment
 def average_rating
   comments.average(:rating).to_f
 end
+def views
+    $redis.get("product:#{id}")
+  end
+
+  def viewed
+    $redis.incr("product:#{id}")
+  end
+
+  def set_recent_reviewer(user_name)
+    $redis.set("rr_product:#{id}","#{user_name}")
+  end
+
+  def get_recent_reviewer
+    $redis.get("rr_product:#{id}")
+  end
+
 end
