c313dd64face19832ebecdb909191113c672cd01
merhawieri <merakassala@gmail.com>
2018-10-15 15:27:01 -0400

    6.13

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ end
   get 'simple_pages/contact'
   get 'simple_pages/index'
 post 'simple_pages/thank_you'
-
 post 'payments/create'
 
   root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
+  mount ActionCable.server => '/cable'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
