17c48081b0c6c8b88582720e7e6032f1fbc25b9b
merhawieri <merakassala@gmail.com>
2018-10-28 01:32:42 -0400

    6,15 rev

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,17 +1,12 @@
-require 'rails_helper'
+equire 'rails_helper'
 
 describe Order do
-  context "when a new order is created" do
-    it "is valid with a user and product" do
-      expect(FactoryBot.build(:order)).to be_valid
-    end
+  context "when the user has many orders" do
+    @order = FactoryBot.create(:order)
 
-    it "is not valid without a product" do
-      expect(FactoryBot.build(:order, product:nil)).not_to be_valid
-    end
+  it "is not valid without a user ID" do
+    expect(@order).to be_nil
+  end
 
-    it "is not valid without a user" do
-      expect(FactoryBot.build(:order, user:nil)).not_to be_valid
-    end
   end
 end
