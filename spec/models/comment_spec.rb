ef8280cc7da0cdd247d34ff4960f1156225f8b92
merhawieri <merakassala@gmail.com>
2018-11-01 00:40:46 -0400

    6.15 rev

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
- describe Comment do
-  context "if product has comments" do
-    let(:product) {
-      Product.create!(
-        name: "Product-test",
-        description: "This is a test ptoduct",
-        image_url: "products/s2.jpg",
-      )
-    }
 
-    let(:user) {User.create!(email: "myrandom@email.com", admin: false, password: "Yellow123")}
+describe Comment do
 
-    it "is invalid without a comment" do
-      expect(Comment.new(rating: 5, user: user)).not_to be_valid
+  context "when product and user exist" do
+    # let(:product) { Product.create!(name: "race bike") }
+    product = FactoryBot.build(:product)
+
+    before do
+      # User.create!(email: "john@doe.com", password: "123456")
+
+      # Have to use create method
+      # Otherwise user won't be stored and thus blank
+      FactoryBot.create(:user)
     end
 
-    it "is invalid with a rating grater than 5" do
-      expect(Comment.new(rating: 6, user: user, body: "invalid comment")).not_to be_valid
+    it "is not valid without a rating" do
+      expect(product.comments.new(product_id: 1, user_id: 1, body: "Nice bike!")).not_to be_valid
     end
+
+    it "is not valid without a body" do
+      expect(product.comments.new(product_id: 1, user_id: 1, rating: 3)).not_to be_valid
+    end
+
+    it "is not valid without a user" do
+      expect(product.comments.new(product_id: 1, rating: 3, body: "Nice bike!")).not_to be_valid
+    end
+
+
+    it "to be valid when all info is given" do
+      expect(product.comments.new(product_id: 1, user_id: 1, rating: 3, body: "Nice bike!")).to be_valid
+    end
+
+
   end
 end
