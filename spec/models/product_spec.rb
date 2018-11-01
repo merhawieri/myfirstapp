684a4c38d6e1131f324685538ea60418dd6d6594
merhawieri <merakassala@gmail.com>
2018-11-01 00:34:49 -0400

    6.15 rev

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
- describe Product do
-   let(:product) {
-     # product_category = Category.create!(product_category: "Sessions", name: "30 Minute Midgrade", colour: "blue", description: "Lifechanging Coaching.", price: 45 )
-     Product.create!(name: "mnbike", colour: "blue", description: "Lifechanging Coaching.", price: "45")
-   }
 
-   let(:user) {User.create!(email: "myrandom@email.com", admin: false, password: "Yellow123")}
+describe Product do
+
+  context "when the product has comments" do
+    # let(:product) { Product.create!(name: "super bike") }
+    product = FactoryBot.create(:product)
+
+    # let(:user) { User.create!(email: "marc@smith.com", password: "123456")}
+    user = FactoryBot.build(:user)
+
 
     before do
-      product.comments.create!(rating: 1, user: user, body: "Bad Session")
-      product.comments.create!(rating: 3, user: user, body: "Ok Session")
-      product.comments.create!(rating: 5, user: user, body: "Great Session!")
+      # product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+      # product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+      # product.comments.create!(rating: 5, user: user, body: "Great bike!")
+      FactoryBot.create(:comment, rating: 1, body: "bad bike", product: product)
+      FactoryBot.create(:comment, rating: 3, product: product)
+      FactoryBot.create(:comment, rating: 5, body: "awesome it is", product: product)
+    end
+
+    it "returns the highest rating of all comments" do
+      expect(product.highest_rating_comment.rating).to eq 5
+      expect(product.highest_rating_comment.body).to eq "awesome it is"
+
+    end
+
+    it "returns the lowest rating of all comments" do
+      expect(product.lowest_rating_comment.rating).to eq 1
+      expect(product.lowest_rating_comment.body).to eq "bad bike"
     end
 
     it "returns the average rating of all comments" do
@@ -18,8 +35,8 @@ require 'rails_helper'
     end
 
     it "is not valid without a name" do
-      product=FactoryBot.build(:product, name: nil)
-      expect(product.valid?).not_to be_truthy
+      expect(Product.new(description: "Nice bike")).not_to be_valid
     end
 
+  end
 end
