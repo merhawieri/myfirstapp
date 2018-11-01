6a6a7963def4959a2e980abcd7940c540dc23785
merhawieri <merakassala@gmail.com>
2018-11-01 01:38:35 -0400

    6.15 rev

--- spec/factories/comment_factory.rb
+++ spec/factories/comment_factory.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
-  factory :comment do
-    body "Great!"
-    rating 5
-    user
-    product
+  sequence(:user_id) { |n| "#{n}" }
+  sequence(:product_id) { |n| "#{n}" }
+
+  factory :comment, class: Comment do
+    rating 5.0
+    body 'amazing'
+    user_id
+    product_id
   end
+
 end
