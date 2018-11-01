0ee0520690a026356a1c33403db5bd15b01c51bc
merhawieri <merakassala@gmail.com>
2018-11-01 12:34:46 -0400

    6.15 rev

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-describe User do
+describe Order do
+  context "when a new order is created" do
+    it "is valid with a user and product" do
+      expect(FactoryBot.build(:order)).to be_valid
+    end
 
-  let(:user) { FactoryBot.create(:user, password: "test123") }
+    it "is not valid without a product" do
+      expect(FactoryBot.build(:order, product:nil)).not_to be_valid
+    end
 
-  it "is a valid user" do
-    expect(user.email).to match (/jim_[0-9\.]+@example.com/)
+    it "is not valid without a user" do
+      expect(FactoryBot.build(:order, user:nil)).not_to be_valid
+    end
   end
-
-  it "is not a valid password" do
-    expect(user.password).to eq "test123"
-   end
 end
