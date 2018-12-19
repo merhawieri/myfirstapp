0dc99897255e97ea2ff591423e3442fe453a1d3c
merhawieri <merakassala@gmail.com>
2018-12-19 12:22:41 -0500

    6.15 rev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ config.app_generators.javascript_engine = :javascript
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Suppress logger output for asset requests.
